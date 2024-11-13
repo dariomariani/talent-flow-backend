ee3a47d4d8c53b5e201691c8aa5f625545e24806
Dario Mariani <dario.mariani@hotmail.it>
2024-11-13 22:33:36 +0100

    initial commit

---
+++ bootstrap/src/main/java/com/dariom/bootstrap/BootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.dariom.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapApplication.class, args);
+	}
+
+}
---
+++ bootstrap/src/test/java/com/dariom/bootstrap/BootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dariom.bootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
